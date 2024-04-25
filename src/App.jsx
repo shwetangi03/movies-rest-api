cdc54d189984e21ab93e9208c646cfed8bd023da
shwetangi03 <113777115+shwetangi03@users.noreply.github.com>
2024-04-25 23:30:05 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useCallback } from "react";
 import MoviesList from "./components/MoviesList";
 import AddMovie from "./components/AddMovie";
 import "./App.css";
-import axios from "axios";
+
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -15,7 +15,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        "https://crudcrud.com/api/ab6b1299b4c842ecb160f8b2d1cb7f59/films"
+        "https://react-rest-api-1dc04-default-rtdb.firebaseio.com/movies.json"
       );
       if (!response.ok) {
         throw new Error("Something went wrong!");
@@ -47,18 +47,41 @@ function App() {
   }, [fetchMoviesHandler]);
 
   async function addMovieHandler(movie) {
-    const response = await axios.post(
-      "https://crudcrud.com/api/ab6b1299b4c842ecb160f8b2d1cb7f59/films",
-      movie
+    const res = await fetch(
+      "https://react-rest-api-1dc04-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "content-type": "application/json",
+        },
+      }
     );
-    const data = response.json;
+    const data = res.json;
     console.log(data);
+    fetchMoviesHandler();
+  }
+
+  async function deleteMovieHandler(movieId) {
+    const res = await fetch(
+      `https://react-rest-api-1dc04-default-rtdb.firebaseio.com/movies/${movieId}.json`,
+      {
+        method: "DELETE",
+        body: JSON.stringify(movieId),
+        headers: {
+          "content-type": "application/json",
+        },
+      }
+    );
+    const data = res.json;
+    console.log(data);
+    fetchMoviesHandler();
   }
 
   let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
-    content = <MoviesList movies={movies} />;
+    content = <MoviesList movies={movies} onDelete={deleteMovieHandler} />;
   }
 
   if (error) {
